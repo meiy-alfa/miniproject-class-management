a4d3928789fcda7857a789d9c16bd805fc5fb4f4
meiy-alfa <miftahalfaruq@gmail.com>
2022-12-14 14:30:39 +0700

    edit routes

--- application/views/login.php
+++ application/views/login.php
@@ -6,7 +6,7 @@
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
         <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <link rel="stylesheet" href="../assets/login_css.css">    
+        <link rel="stylesheet" href="<?= site_url('assets') ?>/login_css.css">    
     </head>
     <body>
         <div id="login">
